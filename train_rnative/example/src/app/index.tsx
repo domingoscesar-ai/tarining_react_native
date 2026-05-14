7546a87aadb963d670ba5ed830c62eab6e8fd237
domingoscesar-ai <domingoscesar2001@gmail.com>
2026-05-14 05:25:08 +0100

    feat: applicated reset-project to clean all unecessary code for our lesson

--- train_rnative/example/src/app/index.tsx
+++ train_rnative/example/src/app/index.tsx
@@ -1,17 +1,18 @@
 import { Text, View, StyleSheet } from "react-native";
 
+
 export default function Index() {
   return (
-    <View style={styles.container}>
+    <View style={style.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
-});
+})
